eed80f713151ee17d807069675ee61e34abfb94d
Francisco Herrera <frherrer@redhat.com>
2024-11-06 17:22:31 -0500

    Update demo

--- IstioDay/IstioDay.sh
+++ IstioDay/IstioDay.sh
@@ -5,14 +5,25 @@ kind create cluster --name istioday
 # For me on MAC using podman: KIND_EXPERIMENTAL_PROVIDER=podman kind create cluster --name istioday
 
 # 2. Install Sail Operator
-## Install the operator using helm install
-# Add the steps here to install the operator using helm
-# Using the make target we will create the chart tarball and used it to install the operator
+BUILD_WITH_CONTAINER=0 make helm-package
+# This will generate the helm chart in the out folder
+# On terminal 1
+helm install sail-operator out/sail-operator-0.2.0.tgz --namespace sail-operator --create-namespace
+kubectl get pods -n sail-operator -w
 
 # 3. Revision based update strategy
-
+# On terminal 1
 kubectl create namespace istio-system
 
+## Watch the Istio resource status
+# On terminal 2
+kubectl get istio -n istio-system -w
+
+## Watch also the revision status
+# On terminal 3
+kubectl get istiorevision -n istio-system -w
+
+# On terminal 1
 cat <<EOF | kubectl apply -f-
 apiVersion: sailoperator.io/v1alpha1
 kind: Istio
@@ -26,52 +37,61 @@ spec:
   version: v1.23.2
 EOF
 
-## Watch the Istio resource status
-kubectl get istio -n istio-system -w
-
-## Watch also the revision status
-kubectl get istiorevision -n istio-system -w
-
-## Show the istio resource created
+## After the watcher show the creation show the istio resource created
+# On terminal 1
 kubectl get istio -n istio-system
 
 ## Create the app ns and the deploy the app
+# On terminal 1
 kubectl create namespace bookinfo
-kubectl label namespace bookinfo istio.io/rev=default-v1-21-0
-
+kubectl label namespace bookinfo istio.io/rev=default-v1-23-2
 kubectl apply -n bookinfo -f https://raw.githubusercontent.com/istio/istio/master/samples/bookinfo/platform/kube/bookinfo.yaml
 
 ## Check the  app pods
+# On terminal 2
 kubectl get pods -n bookinfo -w
 
 ## Check the proxy version
+# On terminal 1
 istioctl proxy-status
 
 ## Update the istio version
+# On terminal 1
 kubectl patch istio default -n istio-system --type='merge' -p '{"spec":{"version":"latest"}}'
 
 ## Check How the new istio and istiorevision resources are created
-kubectl get istio -n istio-system
-kubectl get istiorevision -n istio-system
+# On terminal 2
+kubectl get istio -n istio-system -w
+# On terminal 3
+kubectl get istiorevision -n istio-system -w 
+
+# On terminal 1
 kubectl get pods -n istio-system
 
 ## Confirm that proxy is not updated yet
+# On terminal 1
 istioctl proxy-status
 
 ## Change the label for the new revision
-kubectl label namespace bookinfo istio.io/rev=default-v1-21-2 --overwrite
+# On terminal 1
+kubectl label namespace bookinfo istio.io/rev=default-latest --overwrite
 
 ## Restart the workloads
+# On terminal 1
 kubectl rollout restart deployment -n bookinfo
 
 ## Check the new proxy version
+# On terminal 1
 istioctl proxy-status
 
 ## Confirm that the old revision is deleted
+# On terminal 1
 kubectl get istiorevision -n istio-system
+# On terminal 1
 kubectl get pods -n istio-system
 
 # 4. Delete everything
+# On terminal 1
 kind delete cluster --name istioday
 
 
